cbe5e716ae33f2a1ae457c4221d12c3998b6540c
Dilahara MMS <41099408+chapayev0@users.noreply.github.com>
2023-06-16 13:29:13 -0700

    complet redaraw and clear previous chart

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 from matplotlib.backends.backend_template import FigureCanvas
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
@@ -19,6 +20,10 @@ from mpl_toolkits.mplot3d import Axes3D
 from ui.ui_main import *
 
 global_state = 0
+legend_val = True
+G = 39.478  # Gravitational constant
+dt = 0.001
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,6 +31,11 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.draw()
 
         def move_window(event):
 
@@ -37,27 +47,106 @@ class MainWindow(QMainWindow):
 
         self.ui.title_bar.mouseMoveEvent = move_window
 
+        #self.plot_show(legend_val, G)
+
         #button comands
         self.ui.close_btn.clicked.connect(self.terminate_fun)
         self.ui.maxmize_btn.clicked.connect(self.maxmize_fun)
         self.ui.minimize_btn.clicked.connect(self.minimize_fun)
-        self.ui.home_btn.clicked.connect(self.plot_show)
+        self.ui.legend_chk.stateChanged.connect(self.legend_val_change)
+       # self.ui.g_edit.textChanged.connect(self.g_val_change)
+        self.ui.home_btn.clicked.connect(self.generate_plot)
+        self.ui.set_btn.clicked.connect(self.remove_layout)
 
-    def plot_test(self):
 
-        fig = Figure(figsize=(7, 5), dpi=65, facecolor=(1, 1, 1), edgecolor=(0, 0, 0))
-        self.canvas = FigureCanvas(fig)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.lay = QVBoxLayout()
-        self.lay.addWidget(self.toolbar)
-        self.lay.addWidget(self.canvas)
-        self.ui.plot_screen.setLayout(self.lay)
 
-    def plot_show(self):
 
-        # Constants (values are in astronomical units, masses in solar masses, and time in years)
-        G = 39.478  # Gravitational constant
-        dt = 0.001  # Time step
+    def remove_layout(self):
+        print("layoyt")
+
+        self.ax.cla()
+        self.ax.relim()  # Recalculate the limits
+        self.ax.autoscale_view()
+        self.canvas.draw()
+
+
+
+
+
+
+    def generate_plot(self):
+        
+        global G
+        global legend_val
+
+        self.g_val_change()
+
+        print(G)
+
+
+        self.plot_show(legend_val, G, dt)
+
+
+
+
+
+    def g_val_change(self):
+
+        global G
+
+        G = float(self.ui.dt_edit.text())
+
+        print(G)
+
+        #self.plot_show(legend_val, G)
+
+    def dt_val_change(self):
+
+        global dt
+
+        dt = float(self.ui.g_edit.text())
+
+        print(dt)
+
+        #self.plot_show(legend_val, G)
+
+
+    def legend_val_change(self):
+
+        global legend_val
+
+        print("sss")
+
+
+
+        if self.ui.legend_chk.isChecked():
+
+            legend_val = True
+
+            print(legend_val)
+
+        else:
+
+            legend_val = False
+            print(legend_val)
+
+        #self.plot_show(legend_val, G
+
+
+
+
+
+    def plot_show(self, legend_val, G, dt):
+
+        # Create a 3D plot
+
+        self.toolbar = NavigationToolbar(self.canvas,self)
+
+        self.ax.cla()
+        self.ax.relim()  # Recalculate the limits
+        self.ax.autoscale_view()
+        self.canvas.draw()
+
 
         # Define the initial conditions (positions, velocities, masses) for the Sun and the planets
         positions = np.array([[0, 0, 0],  # Sun
@@ -119,41 +208,51 @@ class MainWindow(QMainWindow):
             # Store the positions in the trajectory array
             trajectory[:, t, :] = positions
 
-        # Create a 3D plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
 
         # Plot the trajectory as elliptical paths and add a legend
         for i in range(num_bodies):
-            ax.plot(trajectory[i, :, 0], trajectory[i, :, 1], trajectory[i, :, 2], label=labels[i])
+
+            self.ax.plot(trajectory[i, :, 0], trajectory[i, :, 1], trajectory[i, :, 2], label=labels[i])
 
             # Plot the last position as a circular marker
-            ax.scatter(trajectory[i, -1, 0], trajectory[i, -1, 1], trajectory[i, -1, 2], s=10, color='black')
+            self.ax.scatter(trajectory[i, -1, 0], trajectory[i, -1, 1], trajectory[i, -1, 2], s=10, color='black')
 
         # Set labels and title
-        ax.set_xlabel('X position (AU)')
-        ax.set_ylabel('Y position (AU)')
-        ax.set_zlabel('Z position (AU)')
-        ax.set_title('Solar System Simulation')
+        self.ax.set_xlabel(self.ui.x_lbl.text())
+        self.ax.set_ylabel(self.ui.y_lbl.text())
+        self.ax.set_zlabel(self.ui.z_lbl.text())
+        #ax.set_title('Solar System Simulation')
 
-        ax.spines['top'].set_visible(True)
-        ax.spines['right'].set_visible(True)
-        ax.spines['bottom'].set_visible(True)
-        ax.spines['left'].set_visible(True)
 
         # Add a legend in the top right corner
-        ax.legend(loc='best')
+
+
+
+        self.ax.legend(loc='best')
+
+        self.ax.legend().set_visible(legend_val)
+        #self.canvas.se
+
 
         # Show the plot
         #plt.show()
 
-        self.canvas = FigureCanvas(fig)
-        self.toolbar = NavigationToolbar(self.canvas, self)
+
+
+
+
         self.lay = QVBoxLayout()
-        self.lay.addWidget(self.toolbar)
+        #self.lay.addWidget(self.toolbar)
         self.lay.addWidget(self.canvas)
-        self.ui.plot_screen.setLayout(self.lay)
-        self.canvas
+
+        self.lay1 = QVBoxLayout()
+        self.lay1.addWidget(self.toolbar)
+
+        self.ui.plot_top.setLayout(self.lay)
+        self.ui.nav_bar.setLayout(self.lay1)
+        #self.ui.plot_screen.setVisible(True)
+
+
 
 
 
--- ui/ui_main.py
+++ ui/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'maindgUQpz.ui'
+## Form generated from reading UI file 'mainuSdPTq.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,24 +17,43 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 485)
+        MainWindow.resize(1016, 642)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(-1, 0, -1, -1)
         self.title_bar = QFrame(self.centralwidget)
         self.title_bar.setObjectName(u"title_bar")
-        self.title_bar.setMinimumSize(QSize(0, 30))
-        self.title_bar.setMaximumSize(QSize(16777215, 30))
+        self.title_bar.setMinimumSize(QSize(0, 35))
+        self.title_bar.setMaximumSize(QSize(16777215, 35))
+        self.title_bar.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.title_bar.setFrameShape(QFrame.StyledPanel)
         self.title_bar.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.title_bar)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.horizontalLayout_3.setContentsMargins(0, 0, -1, -1)
-        self.horizontalSpacer = QSpacerItem(705, 16, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalLayout_3.setContentsMargins(0, 6, -1, 6)
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+        self.title_lbl = QLabel(self.title_bar)
+        self.title_lbl.setObjectName(u"title_lbl")
+        font = QFont()
+        font.setFamily(u"Poppins Medium")
+        font.setPointSize(9)
+        font.setBold(False)
+        font.setWeight(50)
+        self.title_lbl.setFont(font)
+
+        self.horizontalLayout_3.addWidget(self.title_lbl)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
 
         self.minimize_btn = QPushButton(self.title_bar)
         self.minimize_btn.setObjectName(u"minimize_btn")
@@ -93,6 +112,7 @@ class Ui_MainWindow(object):
         self.frame_3 = QFrame(self.frame_2)
         self.frame_3.setObjectName(u"frame_3")
         self.frame_3.setMaximumSize(QSize(50, 16777215))
+        self.frame_3.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.frame_3.setFrameShape(QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QFrame.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.frame_3)
@@ -102,12 +122,48 @@ class Ui_MainWindow(object):
         self.home_btn.setObjectName(u"home_btn")
         self.home_btn.setMinimumSize(QSize(50, 50))
         self.home_btn.setMaximumSize(QSize(50, 50))
+        self.home_btn.setStyleSheet(u"QPushButton{\n"
+"\n"
+"border:none;\n"
+"\n"
+"background-color: rgb(171, 108, 130);\n"
+"	\n"
+"	\n"
+"	\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"\n"
+"background-color: rgb(216, 115, 127);\n"
+"	\n"
+"	\n"
+"	\n"
+"}\n"
+"")
 
         self.verticalLayout_2.addWidget(self.home_btn)
 
         self.set_btn = QPushButton(self.frame_3)
         self.set_btn.setObjectName(u"set_btn")
         self.set_btn.setMinimumSize(QSize(50, 50))
+        self.set_btn.setStyleSheet(u"QPushButton{\n"
+"\n"
+"border:none;\n"
+"\n"
+"background-color: rgb(171, 108, 130);\n"
+"	\n"
+"	\n"
+"	\n"
+"}\n"
+"\n"
+"QPushButton:hover{\n"
+"\n"
+"background-color: rgb(216, 115, 127);\n"
+"	\n"
+"	\n"
+"	\n"
+"}\n"
+"")
 
         self.verticalLayout_2.addWidget(self.set_btn)
 
@@ -120,15 +176,20 @@ class Ui_MainWindow(object):
 
         self.frame_4 = QFrame(self.frame_2)
         self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setStyleSheet(u"background-color: rgb(239, 238, 238);")
         self.frame_4.setFrameShape(QFrame.StyledPanel)
         self.frame_4.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_2 = QHBoxLayout(self.frame_4)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.frame_5 = QFrame(self.frame_4)
         self.frame_5.setObjectName(u"frame_5")
-        self.frame_5.setMinimumSize(QSize(100, 0))
-        self.frame_5.setMaximumSize(QSize(200, 16777215))
-        self.frame_5.setStyleSheet(u"background-color: rgb(217, 217, 217);")
+        self.frame_5.setMinimumSize(QSize(200, 0))
+        self.frame_5.setMaximumSize(QSize(250, 16777215))
+        self.frame_5.setStyleSheet(u"QFrame{\n"
+"\n"
+"	background-color: rgb(233, 233, 233);\n"
+"\n"
+"}")
         self.frame_5.setFrameShape(QFrame.StyledPanel)
         self.frame_5.setFrameShadow(QFrame.Raised)
         self.verticalLayout_3 = QVBoxLayout(self.frame_5)
@@ -146,6 +207,9 @@ class Ui_MainWindow(object):
         self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.label = QLabel(self.frame_8)
         self.label.setObjectName(u"label")
+        self.label.setMinimumSize(QSize(0, 20))
+        self.label.setStyleSheet(u"background-color: rgb(252, 187, 109);\n"
+"")
         self.label.setAlignment(Qt.AlignCenter)
 
         self.horizontalLayout_4.addWidget(self.label)
@@ -155,8 +219,232 @@ class Ui_MainWindow(object):
 
         self.frame_7 = QFrame(self.frame_5)
         self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.frame_7.setFrameShape(QFrame.StyledPanel)
         self.frame_7.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.frame_7)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.frame_7)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.legend_chk = QCheckBox(self.frame)
+        self.legend_chk.setObjectName(u"legend_chk")
+        self.legend_chk.setStyleSheet(u"background:none;")
+        self.legend_chk.setChecked(True)
+
+        self.verticalLayout_5.addWidget(self.legend_chk)
+
+        self.frame_10 = QFrame(self.frame)
+        self.frame_10.setObjectName(u"frame_10")
+        self.frame_10.setFrameShape(QFrame.StyledPanel)
+        self.frame_10.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_10)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.label_2 = QLabel(self.frame_10)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_5.addWidget(self.label_2)
+
+        self.g_edit = QLineEdit(self.frame_10)
+        self.g_edit.setObjectName(u"g_edit")
+        self.g_edit.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.g_edit.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_5.addWidget(self.g_edit)
+
+
+        self.verticalLayout_5.addWidget(self.frame_10)
+
+        self.frame_11 = QFrame(self.frame)
+        self.frame_11.setObjectName(u"frame_11")
+        self.frame_11.setFrameShape(QFrame.StyledPanel)
+        self.frame_11.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_11)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.frame_11)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout_6.addWidget(self.label_3)
+
+        self.dt_edit = QLineEdit(self.frame_11)
+        self.dt_edit.setObjectName(u"dt_edit")
+        self.dt_edit.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.dt_edit.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_6.addWidget(self.dt_edit)
+
+
+        self.verticalLayout_5.addWidget(self.frame_11)
+
+        self.x_lbl = QLineEdit(self.frame)
+        self.x_lbl.setObjectName(u"x_lbl")
+        self.x_lbl.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.x_lbl.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_5.addWidget(self.x_lbl)
+
+        self.y_lbl = QLineEdit(self.frame)
+        self.y_lbl.setObjectName(u"y_lbl")
+        self.y_lbl.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.y_lbl.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_5.addWidget(self.y_lbl)
+
+        self.z_lbl = QLineEdit(self.frame)
+        self.z_lbl.setObjectName(u"z_lbl")
+        self.z_lbl.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.z_lbl.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_5.addWidget(self.z_lbl)
+
+
+        self.verticalLayout_4.addWidget(self.frame)
+
+        self.frame_6 = QFrame(self.frame_7)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setMinimumSize(QSize(0, 50))
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_7 = QVBoxLayout(self.frame_6)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.label_7 = QLabel(self.frame_6)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setMinimumSize(QSize(0, 20))
+        self.label_7.setStyleSheet(u"background-color: rgb(252, 187, 109);\n"
+"")
+        self.label_7.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_7.addWidget(self.label_7)
+
+        self.obj_name = QLineEdit(self.frame_6)
+        self.obj_name.setObjectName(u"obj_name")
+        self.obj_name.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_7.addWidget(self.obj_name)
+
+        self.frame_14 = QFrame(self.frame_6)
+        self.frame_14.setObjectName(u"frame_14")
+        self.frame_14.setFrameShape(QFrame.StyledPanel)
+        self.frame_14.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_7 = QHBoxLayout(self.frame_14)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.label_4 = QLabel(self.frame_14)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_7.addWidget(self.label_4)
+
+        self.pe1 = QLineEdit(self.frame_14)
+        self.pe1.setObjectName(u"pe1")
+
+        self.horizontalLayout_7.addWidget(self.pe1)
+
+        self.pe2 = QLineEdit(self.frame_14)
+        self.pe2.setObjectName(u"pe2")
+
+        self.horizontalLayout_7.addWidget(self.pe2)
+
+        self.pe3 = QLineEdit(self.frame_14)
+        self.pe3.setObjectName(u"pe3")
+
+        self.horizontalLayout_7.addWidget(self.pe3)
+
+
+        self.verticalLayout_7.addWidget(self.frame_14)
+
+        self.frame_12 = QFrame(self.frame_6)
+        self.frame_12.setObjectName(u"frame_12")
+        self.frame_12.setFrameShape(QFrame.StyledPanel)
+        self.frame_12.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_8 = QHBoxLayout(self.frame_12)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.horizontalLayout_8.setContentsMargins(0, 0, 0, 0)
+        self.label_5 = QLabel(self.frame_12)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_8.addWidget(self.label_5)
+
+        self.ve1 = QLineEdit(self.frame_12)
+        self.ve1.setObjectName(u"ve1")
+
+        self.horizontalLayout_8.addWidget(self.ve1)
+
+        self.ve2 = QLineEdit(self.frame_12)
+        self.ve2.setObjectName(u"ve2")
+
+        self.horizontalLayout_8.addWidget(self.ve2)
+
+        self.ve3 = QLineEdit(self.frame_12)
+        self.ve3.setObjectName(u"ve3")
+
+        self.horizontalLayout_8.addWidget(self.ve3)
+
+
+        self.verticalLayout_7.addWidget(self.frame_12)
+
+        self.frame_13 = QFrame(self.frame_6)
+        self.frame_13.setObjectName(u"frame_13")
+        self.frame_13.setFrameShape(QFrame.StyledPanel)
+        self.frame_13.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_9 = QHBoxLayout(self.frame_13)
+        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
+        self.horizontalLayout_9.setContentsMargins(0, 0, 0, 0)
+        self.label_6 = QLabel(self.frame_13)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_9.addWidget(self.label_6)
+
+        self.mass_edit = QLineEdit(self.frame_13)
+        self.mass_edit.setObjectName(u"mass_edit")
+
+        self.horizontalLayout_9.addWidget(self.mass_edit)
+
+
+        self.verticalLayout_7.addWidget(self.frame_13)
+
+        self.add_btn = QPushButton(self.frame_6)
+        self.add_btn.setObjectName(u"add_btn")
+        self.add_btn.setMinimumSize(QSize(0, 20))
+        self.add_btn.setStyleSheet(u"QPushButton{\n"
+"\n"
+"border:none;\n"
+"	background-color: rgb(74, 255, 71);}\n"
+"")
+
+        self.verticalLayout_7.addWidget(self.add_btn)
+
+
+        self.verticalLayout_4.addWidget(self.frame_6)
+
+        self.frame_9 = QFrame(self.frame_7)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setMinimumSize(QSize(0, 50))
+        self.frame_9.setFrameShape(QFrame.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_8 = QVBoxLayout(self.frame_9)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.obj_list = QListWidget(self.frame_9)
+        self.obj_list.setObjectName(u"obj_list")
+
+        self.verticalLayout_8.addWidget(self.obj_list)
+
+        self.remove_btn = QPushButton(self.frame_9)
+        self.remove_btn.setObjectName(u"remove_btn")
+        self.remove_btn.setStyleSheet(u"background-color: rgb(255, 15, 79);")
+
+        self.verticalLayout_8.addWidget(self.remove_btn)
+
+
+        self.verticalLayout_4.addWidget(self.frame_9)
+
 
         self.verticalLayout_3.addWidget(self.frame_7)
 
@@ -165,8 +453,26 @@ class Ui_MainWindow(object):
 
         self.plot_screen = QFrame(self.frame_4)
         self.plot_screen.setObjectName(u"plot_screen")
+        self.plot_screen.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.plot_screen.setFrameShape(QFrame.StyledPanel)
         self.plot_screen.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.plot_screen)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.nav_bar = QFrame(self.plot_screen)
+        self.nav_bar.setObjectName(u"nav_bar")
+        self.nav_bar.setMaximumSize(QSize(16777215, 50))
+        self.nav_bar.setFrameShape(QFrame.StyledPanel)
+        self.nav_bar.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout_6.addWidget(self.nav_bar)
+
+        self.plot_top = QFrame(self.plot_screen)
+        self.plot_top.setObjectName(u"plot_top")
+        self.plot_top.setFrameShape(QFrame.StyledPanel)
+        self.plot_top.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout_6.addWidget(self.plot_top)
+
 
         self.horizontalLayout_2.addWidget(self.plot_screen)
 
@@ -185,6 +491,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.title_lbl.setText(QCoreApplication.translate("MainWindow", u"Many Body Simulator", None))
 #if QT_CONFIG(tooltip)
         self.minimize_btn.setToolTip(QCoreApplication.translate("MainWindow", u"Minimize", None))
 #endif // QT_CONFIG(tooltip)
@@ -200,5 +507,25 @@ class Ui_MainWindow(object):
         self.home_btn.setText("")
         self.set_btn.setText("")
         self.label.setText(QCoreApplication.translate("MainWindow", u"Plot Settings", None))
+        self.legend_chk.setText(QCoreApplication.translate("MainWindow", u"Legend", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"G =", None))
+        self.g_edit.setText(QCoreApplication.translate("MainWindow", u"39.478", None))
+        self.g_edit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Gravitatonal Constnt(G)", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"dt =", None))
+        self.dt_edit.setText(QCoreApplication.translate("MainWindow", u"0.001", None))
+        self.dt_edit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Time Step(dt)", None))
+        self.x_lbl.setText(QCoreApplication.translate("MainWindow", u"X Position", None))
+        self.x_lbl.setPlaceholderText(QCoreApplication.translate("MainWindow", u"X Label", None))
+        self.y_lbl.setText(QCoreApplication.translate("MainWindow", u"Y Position", None))
+        self.y_lbl.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Y Label", None))
+        self.z_lbl.setText(QCoreApplication.translate("MainWindow", u"Z Position", None))
+        self.z_lbl.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Z Label", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Object data", None))
+        self.obj_name.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Object Name", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"positions", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"velocities", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"masses", None))
+        self.add_btn.setText(QCoreApplication.translate("MainWindow", u"ADD", None))
+        self.remove_btn.setText(QCoreApplication.translate("MainWindow", u"Remove", None))
     # retranslateUi
 
